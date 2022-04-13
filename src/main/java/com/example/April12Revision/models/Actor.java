8f126dc72af080c6c9a195629c75cce6c3ef3996
Arian Sani <ariansani@hotmail.com>
2022-04-13 17:47:25 +0800

    added model Actor Movie

--- src/main/java/com/example/April12Revision/models/Actor.java
+++ src/main/java/com/example/April12Revision/models/Actor.java
@@ -1,5 +1,24 @@
 package com.example.April12Revision.models;
 
 public class Actor {
-    
+
+    private Integer actorId;
+    private Integer actorName;
+
+    public Integer getActorId() {
+        return actorId;
+    }
+
+    public void setActorId(Integer actorId) {
+        this.actorId = actorId;
+    }
+
+    public Integer getActorName() {
+        return actorName;
+    }
+
+    public void setActorName(Integer actorName) {
+        this.actorName = actorName;
+    }
+
 }
--- src/main/java/com/example/April12Revision/models/Movie.java
+++ src/main/java/com/example/April12Revision/models/Movie.java
@@ -1,7 +1,53 @@
 package com.example.April12Revision.models;
 
-public class Movie {
-    
+import java.util.Date;
 
-    
+public class Movie {
+
+    private Integer movieId;
+    private String movieName;
+    private Integer personalRating;
+    private Date releaseDate;
+    private String synopsis;
+
+    public Integer getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Integer movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public Integer getPersonalRating() {
+        return personalRating;
+    }
+
+    public void setPersonalRating(Integer personalRating) {
+        this.personalRating = personalRating;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
 }
